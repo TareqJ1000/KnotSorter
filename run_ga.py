522d87aa02252a7fa019dab42135884bd3d73617
Tareq Jaouni Jaouni <jaounitareq@yahoo.ca>
2025-12-19 09:39:09 -0500

    modified run_ga to use the crosstalk metric after the initial generation step

--- run_ga.py
+++ run_ga.py
@@ -637,7 +637,7 @@ ga_instance_sorting.run()
 
 ga_instance_crosstalk= pygad.GA(num_generations=num_generations,
                        num_parents_mating=num_parents_mating,
-                       fitness_func=fitness_func_secretKey,
+                       fitness_func=fitness_func_secretKey_crosstalk,
                        sol_per_pop=sol_per_pop,
                        num_genes=num_genes,
                        init_range_low=init_range_low,
